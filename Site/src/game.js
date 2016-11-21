d3f8508607eb251b9afb04120380e8077d4f3df0
Maxime Gammaitoni <m.gammaitoni@student.isartdigital.com>
2016-11-21 15:36:00 +0100

    push d'une version avec image qui charge pas

--- Site/src/game.js
+++ Site/src/game.js
@@ -1,5 +1,5 @@
 //*Variable Global
-var test=0;
+var game;
 window.onload =init()
 function init()
 {
@@ -7,16 +7,23 @@ function init()
 	var width=window.innerWidth;
 	var game = new Phaser.Game(width, height, Phaser.AUTO, "canvas", {preload: preload, create: create, update: update});
 
-}
-function preload()
-{
-	console.log("preload OK");
-}
-function create()
-{
-	console.log("create OK");
-}
-function update()
-{
+	function preload()
+	{ 
+		game.load.image("gearIcon","assets/img/gear.png")//creation de l'image avec l'id "gearIcon" et son url.
+		
+		console.log("preload OK");
+	}
+	function create()
+	{
+		game.stage.backgroundColor = "#4488AA"; // change la couleur de fond du canvas
+		game.add.sprite(0,0,"gearIcon");//ajout de l'image avec l'id gearIcon
+		
+		console.log("create OK");
+	}
+	function update()
+	{
+
+	}
 
 }
+
